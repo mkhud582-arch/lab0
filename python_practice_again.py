160a85eee19a6e931c162932bf2de2e44ad2ff08
mkhud582-arch <mkhud582@mtroyal.ca>
2026-09-13 09:43:56 -0600

    Update word removal logic for case sensitivity
    Fix logic to check for lowercase 'eric' and remove it from input.

--- python_practice_again.py
+++ python_practice_again.py
@@ -3,15 +3,12 @@
 # Eric is considered offensive 
 
 word_input = input("Enter a word: ")
-split_word = word_input.split() 
-capital_word_input = word_input.upper()
 lowercase_word_input = word_input.lower()
 
 
-if split_word == "Eric":
-    if capital_word_input == True:
-        remove_word = split_word.pop()
-        if lowercase_word_input == True:
-            remove_word = split_word.pop()
+if lowercase_word_input == "eric":
+    remove_word = lowerrcase_word_input.pop()
+    print(remove_word)
+        
 
             
